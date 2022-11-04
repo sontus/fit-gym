b0feef91628cf59de0c6000b7a56b300862480ce
Sontus Chandra Anik <info.sontus@gmail.com>
2022-11-04 10:53:56 +0600

    Main Style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
+import Gallery from './pages/gallery/Gallery'
+import Plans from './pages/plans/Plans'
+import Trainers from './pages/trainers/Trainers'
+import NotFound from './pages/notFound/NotFound'
+import Navbar from './components/Navbar'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Navbar></Navbar>
+      <Home/>
+      <About/>
+      <Contact/>
+      <Plans/>
+      <Trainers/>
+      <Gallery/>
+      <NotFound/>
+    </BrowserRouter>
   )
 }
 
